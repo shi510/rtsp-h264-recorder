9ccc75379de514794130bf968e660b04f75c58da
Im Sunghoon <shi510.kor@gmail.com>
2020-02-27 22:57:31 +0900

    add example build enable option, default is off.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,8 @@
-include(cmake/find_ffmpeg.cmake)
-if(FFMPEG_FOUND)
-    list(APPEND vr_include_dirs ${FFMPEG_INCLUDE_DIR})
-    list(APPEND vr_lib_deps ${FFMPEG_LIBRARIES})
+
+if(BUILD_EXAMPLE)
+	include(cmake/find_ffmpeg.cmake)
+	if(FFMPEG_FOUND)
+		list(APPEND vr_include_dirs ${FFMPEG_INCLUDE_DIR})
+		list(APPEND vr_lib_deps ${FFMPEG_LIBRARIES})
+	endif()
 endif()
